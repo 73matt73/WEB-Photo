510444952e37d67b5cc3fe99098030f63da5b41e
73matt73 <133159814+73matt73@users.noreply.github.com>
2026-03-01 21:01:03 +0200

    Fix typo in EMOTIONS_UKR object key

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const statusDiv = document.getElementById('status');
 const imageContainer = document.getElementById('imageContainer');
 
 const EMOTIONS_UKR = {
-    angry: 'Злість',
+    angryy: 'Злість',
     disgusted: 'Огида',
     fearful: 'Страх',
     happy: 'Радість',
@@ -92,3 +92,4 @@ imageUpload.addEventListener('change', async () => {
     statusDiv.style.color = "#065f46";
 
 });
+
